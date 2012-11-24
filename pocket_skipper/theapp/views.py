eaf4df68b952279e4a6c2d9d6ecc81588c7ead3c
Sveder <ms@svedersoft.com>
2012-11-25 00:43:38 +0200

    Bug fixes. And a lesson in why not use global variables.

--- pocket_skipper/theapp/views.py
+++ pocket_skipper/theapp/views.py
@@ -19,8 +19,6 @@ POCKET_OAUTH_CONSUMER_KEY = "PUT YOUR OWN"
 
 POCKET_REDIRECT_URL = "http://%s/skipper" # % domain_url
 
-g_pocket_code = None
-
 def _log(message, _pprint=False, _traceback=False):
     if _pprint:
         pprint.pprint(message)
@@ -40,15 +38,16 @@ def _post(url, data):
 
 
 def landing(request):
-    global g_pocket_code
     redirect_url = POCKET_REDIRECT_URL % request.get_host()
     data = {"redirect_uri" : redirect_url,
             "consumer_key" : POCKET_OAUTH_CONSUMER_KEY}
     
     content, response = _post(POCKET_OAUTH_REQUEST_URL, data)
-    g_pocket_code = dict(urlparse.parse_qsl(response))["code"]
     
-    auth_url = "https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s" % (g_pocket_code, redirect_url)
+    pocket_code = dict(urlparse.parse_qsl(response))["code"]
+    request.session["pocket_code"] = pocket_code
+    
+    auth_url = "https://getpocket.com/auth/authorize?request_token=%s&redirect_uri=%s" % (pocket_code, redirect_url)
     return shortcuts.redirect(auth_url)
 
 
@@ -59,8 +58,11 @@ def skipper(request):
     
     #Try to reauth the user if there isn't a token:
     else:
+        if "pocket_code" not in request.session:
+            return shortcuts.redirect("/pocket")
         try:
-            data = {"code" : g_pocket_code,
+            pocket_code = request.session["pocket_code"]
+            data = {"code" : pocket_code,
                     "consumer_key" : POCKET_OAUTH_CONSUMER_KEY}
             content, response = _post(POCKET_OAUTH_AUTHORIZE_URL, data)
             
@@ -80,6 +82,8 @@ def skipper(request):
     
     reading_list_data = json.loads(response)
     reading_list = reading_list_data["list"]
+    if reading_list == []:
+        reading_list = {}
     
     items = reading_list.values()
     #Sort by date:
